ecb668005890db4859f7cbfbb2cd5ec89252a95c
Al Jack <alistair@alistairjack.com>
2023-11-30 22:43:46 +0700

    big changes

---
+++ script.js
@@ -0,0 +1,23 @@
+// const menuContainer = document.querySelector('.menu-container');
+// const menuNav = document.querySelector('.menu');
+// const subMenu = document.querySelector('.submenu');
+
+// menuContainer.addEventListener('mouseenter', () => {
+//     subMenu.classList.remove('hidden');
+// })
+
+// menuContainer.addEventListener('mouseleave', () => {
+//     subMenu.classList.add('hidden');
+// })
+
+// menuNav.addEventListener('mouseenter', () => {
+//     subMenu.classList.remove('hidden');
+// })
+
+// menuNav.addEventListener('mouseleave', () => {
+//     subMenu.classList.add('hidden');
+// })
+
+// subMenu.addEventListener('mouseleave', () => {
+//     subMenu.classList.add('hidden');
+// })
